bbe7979cf54c1e5eba5f9a07a951ebaaeef93fb8
Shalior <aliali.t2y@gmail.com>
2021-07-05 22:08:32 +0430

    format

--- src/PersianValidationServiceProvider.php
+++ src/PersianValidationServiceProvider.php
@@ -17,43 +17,42 @@ class PersianValidationServiceProvider extends ServiceProvider
      * @var array
      */
     private $validationRules = [
-        'persian_alpha'                 => 'Alpha',
-        'persian_num'                   => 'Num',
-        'persian_alpha_num'             => 'AlphaNum',
-        'iran_mobile'                   => 'IranMobile',
-        'sheba'                         => 'Sheba',
-        'melli_code'                    => 'MelliCode',
-        'is_not_persian'                => 'IsNotPersian',
-        'limited_array'                 => 'LimitedArray',
-        'unsigned_num'                  => 'UnSignedNum',
-        'alpha_space'                   => 'AlphaSpace',
-        'a_url'                         => 'Url',
-        'a_domain'                      => 'Domain',
-        'more'                          => 'More',
-        'less'                          => 'Less',
-        'iran_phone'                    => 'IranPhone',
-        'iran_phone_with_area_code'     => 'IranPhoneWithAreaCode',
-        'card_number'                   => 'CardNumber',
-        'address'                       => 'Address',
-        'iran_postal_code'              => 'IranPostalCode',
-        'package_name'                  => 'PackageName',
+        'persian_alpha'             => 'Alpha',
+        'persian_num'               => 'Num',
+        'persian_alpha_num'         => 'AlphaNum',
+        'iran_mobile'               => 'IranMobile',
+        'sheba'                     => 'Sheba',
+        'melli_code'                => 'MelliCode',
+        'is_not_persian'            => 'IsNotPersian',
+        'limited_array'             => 'LimitedArray',
+        'unsigned_num'              => 'UnSignedNum',
+        'alpha_space'               => 'AlphaSpace',
+        'a_url'                     => 'Url',
+        'a_domain'                  => 'Domain',
+        'more'                      => 'More',
+        'less'                      => 'Less',
+        'iran_phone'                => 'IranPhone',
+        'iran_phone_with_area_code' => 'IranPhoneWithAreaCode',
+        'card_number'               => 'CardNumber',
+        'address'                   => 'Address',
+        'iran_postal_code'          => 'IranPostalCode',
+        'package_name'              => 'PackageName',
     ];
 
     /**
      * create custom validation rules and messages
-     * @author Shahrokh Niakan <sh.niakan@anetwork.ir>
-     * @since May 25, 2016
      * @return void
+     * @since May 25, 2016
+     * @author Shahrokh Niakan <sh.niakan@anetwork.ir>
      */
     public function boot()
     {
         // publish lang file to resources/lang/validation
         $this->publishes([
-            __DIR__ . '/../lang/' . App::getLocale() . '.php' => resource_path('lang/validation/' . App::getLocale() . '.php'),
+            __DIR__.'/../lang/'.App::getLocale().'.php' => resource_path('lang/validation/'.App::getLocale().'.php'),
         ]);
 
-        foreach($this->validationRules as $name => $method)
-        {
+        foreach ($this->validationRules as $name => $method) {
             Validator::extend($name, 'ValidationRules@'.$method);
 
             Validator::replacer($name, 'ValidationMessages@Msg');
@@ -63,9 +62,9 @@ class PersianValidationServiceProvider extends ServiceProvider
 
     /**
      * register PersianValidation service
-     * @author Shahrokh Niakan <sh.niakan@anetwork.ir>
-     * @since May 31, 2016
      * @return void
+     * @since May 31, 2016
+     * @author Shahrokh Niakan <sh.niakan@anetwork.ir>
      */
     public function register()
     {
